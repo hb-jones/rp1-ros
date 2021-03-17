3806c35668783e939a6da22a1c21b2ea40607ee1
Harrison Jones <harrisonbjones@gmail.com>
2021-03-17 15:34:55 +1300

    increase update rate

--- src/rp1controller/trajectory_planners.py
+++ src/rp1controller/trajectory_planners.py
@@ -103,7 +103,7 @@ class WorldPoseControl(ControlMode):
     thread_handle = None 
     target: Target = None #Replace with coordinate and heading? TODO
 
-    delay_time = 0.5
+    delay_time = 0.1
 
     current_target_linear_velocity = (0,0)
     current_target_angular_velocity = 0
