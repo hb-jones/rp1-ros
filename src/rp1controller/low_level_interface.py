b868d0952f6a6aff8cf2abef69ab868b1a2dc36c
Harrison Jones <harrisonbjones@gmail.com>
2021-04-14 11:20:00 +1200

    Remove Axis type checking

--- src/rp1controller/low_level_interface.py
+++ src/rp1controller/low_level_interface.py
@@ -119,7 +119,7 @@ class LowLevelInterface():
         t_motors = self.model.transform_velocity_base_to_motor(t_linear[0], t_linear[1], t_angular)
 
         for axis_name in self.axes_dict:
-            axis: Axis = self.axes_dict[axis_name]
+            axis = self.axes_dict[axis_name]
             target_rad = t_motors[axis_name]
             target_rps = self.radians_to_rps(target_rad)
             if log: self.logger.debug("{name}: - Setting target to {tar}".format(name = axis_name, tar = target_rps))
@@ -129,7 +129,8 @@ class LowLevelInterface():
             print(f"controller.input_vel time: {end_time}")
             self.target_motor[axis_name] = target_rps
             start_time = time.perf_counter()
-            if (abs(axis.controller.input_vel) > abs(target_rps)+0.1 or abs(axis.controller.input_vel) < abs(target_rps) - 0.1):
+            measured_abs_input_vel = abs(axis.controller.input_vel)
+            if (measured_abs_input_vel > abs(target_rps)+0.1 or measured_abs_input_vel < abs(target_rps) - 0.1):
                 self.logger.error("{name} - Target Error: Unexpected velocity input. Expected: {t}, currently: {c}".format(name = axis_name, t = target_rps, c = axis.controller.input_vel))
                 successful = False
             end_time = time.perf_counter()-start_time
@@ -207,7 +208,7 @@ class LowLevelInterface():
             drive = odrive.find_any(serial_number=serial)
             self.logger.debug(" - Odrive {} connected successfully".format(serial))
             return drive
-        def get_axis(drive: Odrv, index: int = 0)->Axis:
+        def get_axis(drive: Odrv, index: int = 0):
             if index == 0:
                 return drive.axis0
             elif index == 1:
@@ -220,7 +221,7 @@ class LowLevelInterface():
         self.odrvF = get_drive(self.config.odrvF_serial_number) #Connect both Odrives
         self.odrvB = get_drive(self.config.odrvB_serial_number)
         
-        self.axes_dict: Dict[Axis] = {} #Convenience dictionary for iterating throgh each axis
+        self.axes_dict = {} #Convenience dictionary for iterating throgh each axis
         self.axis_FL = get_axis(self.odrvF, 1) ; self.axes_dict.update({"axis_FL" : self.axis_FL}) #Get each axis, assign handle and add to dict
         self.axis_FR = get_axis(self.odrvF, 0) ; self.axes_dict.update({"axis_FR" : self.axis_FR}) 
         self.axis_BL = get_axis(self.odrvB, 0) ; self.axes_dict.update({"axis_BL" : self.axis_BL})
