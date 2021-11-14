87abcd5b624688da629a91147a8496cca677bb30
Harrison Jones <harrisonbjones@gmail.com>
2021-11-15 11:53:35 +1300

    Add rate limit to catch

--- src/rp1_fsm_mono.py
+++ src/rp1_fsm_mono.py
@@ -10,14 +10,6 @@ class RP1_MonoFSM(rp1_fsm.RP1_FSM):
             self.disarm = False
             self.logger.info("state: IDLE")
             return rp1_fsm.STATE_IDLE
-        if not self.check_throw_validity():
-            self.logger.info("invalid_throw: Returning home")
-            home_target = Target()
-            self.server.command_set_target(home_target)
-            time.sleep(0.5)
-            self.arm = False
-            self.logger.info("state: IDLE")
-            return rp1_fsm.STATE_IDLE
         if self.trajectory_est.get_last_update_age()>self.limit_update_age:
             self.logger.info("state: PITBULL")
             self.pitbull_time = time.time()
